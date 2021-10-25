110c308bf09fc066f20039970c57567aa282257f
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-10-25 21:36:02 +0900

    add swiftui file

---
+++ SwiftUIKit/TestView.swift
@@ -0,0 +1,20 @@
+//
+//  TestView.swift
+//  SwiftUIKit
+//
+//  Created by 宮本光直 on 2021/10/25.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text("SwftUI")
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
