a0fd113c0ef11bf682a575a72fb7824512358391
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-10-25 21:40:15 +0900

    add button

--- SwiftUIKit/ViewController.swift
+++ SwiftUIKit/ViewController.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func SwiftUIButtonTapped(_ sender: Any) {
+        print(":::: swiftUIの画面へ遷移")
+        let vc = UIHostingController(rootView: TestView())
+        self.present(vc, animated: true)
+    }
 
 }
 
